789f2054b5673bae61da524b0793710b1eaf20d9
Jason Barr <jason@jasonsbarr.com>
2023-06-24 20:01:53 -0500

    Use correct file name in import

--- src/shared/index.js
+++ src/shared/index.js
@@ -1,7 +1,7 @@
 import * as Point from "./Point.js";
 import { XPlace } from "./XPlace.js";
 import { YPlace } from "./YPlace.js";
-import { FillMode } from "./types.js";
+import { FillMode } from "./FillMode.js";
 import { FontFamily } from "./FontFamily.js";
 import { FontStyle } from "./FontStyle.js";
 import { FontWeight } from "./FontWeight.js";
