3f3ef6e74eedb60e90af6de5b65ba77186524bf3
Jason Barr <jason@jasonsbarr.com>
2023-06-24 19:27:08 -0500

    Create FillMode type

--- src/shared/types.js
+++ src/shared/types.js
@@ -57,3 +57,44 @@ export const YPlace = {
   YBaseline: "YBaseline",
   YBottom: "YBottom",
 };
+
+/**
+ * @typedef ModeSolid
+ * @prop {"mode-solid"} type
+ */
+/**
+ * @typedef ModeOutline
+ * @prop {"mode-outline"} type
+ */
+/**
+ * @typedef ModeFade
+ * @prop {"mode-fade"} type
+ * @prop {number} n
+ */
+/**
+ * @typedef {ModeSolid|ModeOutline|ModeFade} FillModeType
+ */
+export const FillMode = {
+  /**
+   * Constructs ModeSolid
+   * @returns {ModeSolid}
+   */
+  Solid() {
+    return { type: "mode-solid" };
+  },
+  /**
+   * Constructs ModeOutline
+   * @returns {ModeOutline}
+   */
+  Outline() {
+    return { type: "mode-outline" };
+  },
+  /**
+   * Constructs ModeFade
+   * @param {number} n
+   * @returns {ModeFade}
+   */
+  Fade(n) {
+    return { type: "mode-fade", n };
+  },
+};
