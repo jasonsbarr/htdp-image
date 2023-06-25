93a6bc0e3bd8bb8a5e8ade38a29a40a8de869753
Jason Barr <jason@jasonsbarr.com>
2023-06-24 20:03:02 -0500

    Move types into type directory

--- src/shared/index.js
+++ src/shared/index.js
@@ -1,10 +1,10 @@
-import * as Point from "./Point.js";
-import { XPlace } from "./XPlace.js";
-import { YPlace } from "./YPlace.js";
-import { FillMode } from "./FillMode.js";
-import { FontFamily } from "./FontFamily.js";
-import { FontStyle } from "./FontStyle.js";
-import { FontWeight } from "./FontWeight.js";
+import * as Point from "./types/Point.js";
+import { XPlace } from "./types/XPlace.js";
+import { YPlace } from "./types/YPlace.js";
+import { FillMode } from "./types/FillMode.js";
+import { FontFamily } from "./types/FontFamily.js";
+import { FontStyle } from "./types/FontStyle.js";
+import { FontWeight } from "./types/FontWeight.js";
 
 export const Types = {
   ...Point,
