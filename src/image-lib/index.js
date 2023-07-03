cf3623353afd2f91ffe3a2cee62886441a1c403c
Jason Barr <jason@jasonsbarr.com>
2023-07-03 14:27:53 -0500

    Export SceneImage from ImageLib

--- src/image-lib/index.js
+++ src/image-lib/index.js
@@ -7,12 +7,15 @@ import {
   XPlace,
   YPlace,
 } from "../shared/index.js";
+import { BaseImage } from "./BaseImage.js";
+import { SceneImage } from "./SceneImage.js";
 
 export const makeColor = Colors.color;
 export const isColor = Colors.isColor;
 
 export {
   BaseImage,
+  SceneImage,
   FillMode,
   FontFamily,
   FontStyle,
