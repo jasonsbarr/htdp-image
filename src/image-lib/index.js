62e2d2a0e4b413bf00ae8949f9db0dcbb446fc55
Jason Barr <jason@jasonsbarr.com>
2023-07-03 20:19:55 -0500

    Import and export new image types

--- src/image-lib/index.js
+++ src/image-lib/index.js
@@ -13,6 +13,10 @@ import { FileImage } from "./types/FileImage.js";
 import { TextImage } from "./types/TextImage.js";
 import { FileVideo } from "./types/FileVideo.js";
 import { ImageDataImage } from "./types/ImageDataImage.js";
+import { OverlayImage } from "./types/OverlayImage.js";
+import { RotateImage } from "./types/RotateImage.js";
+import { EllipseImage } from "./types/EllipseImage.js";
+import { CropImage } from "./types/CropImage.js";
 
 export const makeColor = Colors.color;
 export const isColor = Colors.isColor;
@@ -24,6 +28,10 @@ export {
   TextImage,
   FileVideo,
   ImageDataImage,
+  OverlayImage,
+  RotateImage,
+  EllipseImage,
+  CropImage,
   FillMode,
   FontFamily,
   FontStyle,
