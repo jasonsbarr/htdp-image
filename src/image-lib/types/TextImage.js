185704dac622afe764dfa66e49e4a08cadc09d8d
Jason Barr <jason@jasonsbarr.com>
2023-07-04 13:58:07 -0500

    Add methods for TextImage class

--- src/image-lib/types/TextImage.js
+++ src/image-lib/types/TextImage.js
@@ -1,3 +1,5 @@
+import equals from "fast-deep-equal/es6/index.js";
+import { colorToSpokenString } from "../utils.js";
 import { BaseImage } from "./BaseImage.js";
 
 let ua = "",
@@ -56,9 +58,76 @@ const getTextDimensions = function (str, font) {
   return result;
 };
 
+/**
+ * An image that is all text
+ * @class
+ * @prop {string} str
+ * @prop {number} size
+ * @prop {string} face
+ * @prop {string} family
+ * @prop {string} weight
+ * @prop {boolean} underline
+ * @prop {string} font
+ */
 export class TextImage extends BaseImage {
-  constructor(str, size, color, face, family, style, weight, underline) {}
+  /**
+   * TextImage constructor
+   * @param {string} str
+   * @param {number} size
+   * @param {string} color
+   * @param {string} face
+   * @param {string} family
+   * @param {string} style
+   * @param {string} weight
+   * @param {boolean} underline
+   */
+  constructor(str, size, color, face, family, style, weight, underline) {
+    super({
+      color,
+      ariaText: `the string ${str}, colored ${colorToSpokenString(
+        color,
+        "solid"
+      )} of size ${size}`,
+    });
 
+    this.str = str;
+    this.size = size;
+    this.face = face;
+    this.family = family;
+    this.style = style === "slant" ? "oblique" : style;
+    this.weight = weight === "light" ? "lighter" : weight;
+    this.underline = underline;
+
+    // NOTE: we *ignore* font-family, as it causes a number of font bugs due the browser inconsistencies
+    // example: "bold italic 20px 'Times', sans-serif".
+    // Default weight is "normal", face is "Arial"
+    this.font = this.style ? `${this.style} ` : "";
+    this.font += this.weight ? `${this.weight} ` : "normal ";
+    this.font += this.size ? `${this.size}px ` : "";
+    this.font += '"' + this.face ? this.face : "Arial" + '" ';
+    this.font += this.family ? `, ${this.family}` : "";
+
+    const metrics = getTextDimensions(str, this.font);
+
+    this.width = metrics.width;
+    this.height = metrics.height;
+    this.alphaBaseline = metrics.ascent + baselineFudge;
+    this.pinholeX = this.width / 2;
+    this.pinholeY = this.alphaBaseline;
+  }
+
+  /**
+   * TextImage constructor
+   * @param {string} str
+   * @param {number} size
+   * @param {string} color
+   * @param {string} face
+   * @param {string} family
+   * @param {string} style
+   * @param {string} weight
+   * @param {boolean} underline
+   * @returns {TextImage}
+   */
   static new(str, size, color, face, family, style, weight, underline) {
     return new TextImage(
       str,
@@ -70,5 +139,58 @@ export class TextImage extends BaseImage {
       weight,
       underline
     );
+  }
+
+  /**
+   * TextImage equality check
+   * @param {BaseImage} other
+   * @returns {boolean}
+   */
+  equals(other) {
+    return (
+      (other instanceof TextImage &&
+        this.str === other.str &&
+        this.size === other.size &&
+        this.face === other.face &&
+        this.family === other.family &&
+        this.style === other.style &&
+        this.weight === other.weight &&
+        this.font === other.font &&
+        this.underline === other.underline &&
+        equals(this.color, other.color)) ||
+      BaseImage.prototype.equals.call(this, other)
+    );
+  }
+
+  /**
+   * Renders a TextImage to screen
+   * @param {CanvasRenderingContext2D} ctx
+   */
+  render(ctx) {
+    ctx.save();
+    ctx.textAlign = "left";
+    ctx.textBaseline = "alphabetic"; // Note: NOT top, so that we can support accented characters
+    ctx.font = this.font;
+
+    // if 'outline' is enabled, use strokeText. Otherwise use fillText
+    ctx.fillStyle = this.outline ? "white" : colorString(this.color);
+    ctx.fillText(this.str, 0, this.alphaBaseline - 1); // handle the baseline offset here
+
+    if (this.outline) {
+      ctx.strokeStyle = colorString(this.color);
+      ctx.strokeText(this.str, 0, this.alphaBaseline - 1);
+    }
+
+    if (this.underline) {
+      ctx.beginPath();
+      ctx.moveTo(0, this.size);
+      // we use this.size, as it is more accurate for underlining than this.height
+      ctx.lineTo(this.width, this.size);
+      ctx.closePath();
+      ctx.strokeStyle = colorString(this.color);
+      ctx.stroke();
+    }
+
+    ctx.restore();
   }
 }
