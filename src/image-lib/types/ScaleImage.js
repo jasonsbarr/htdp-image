6deda1701e494763910b25301de83d7d80bee102
Jason Barr <jason@jasonsbarr.com>
2023-07-03 20:04:12 -0500

    Create ScaleImage class

---
+++ src/image-lib/types/ScaleImage.js
@@ -0,0 +1,96 @@
+import { BaseImage } from "./BaseImage.js";
+
+/**
+ * Scale an image
+ * @class
+ * @prop {number} xFactor
+ * @prop {number} yFactor
+ * @prop {BaseImage} img`
+ */
+export class ScaleImage extends BaseImage {
+  /**
+   * Constructor for ScaleImage
+   * @param {number} xFactor
+   * @param {number} yFactor
+   * @param {BaseImage} img
+   */
+  constructor(xFactor, yFactor, img) {
+    super({
+      width: img.width * Math.abs(xFactor),
+      height: img.height * Math.abs(yFactor),
+      pinholeX: img.pinholeX * xFactor,
+      pinholeY: img.pinholeY * yFactor,
+      ariaText: `Scaled image, ${
+        xFactor === yFactor
+          ? `by ${xFactor}`
+          : `horizontally by ${xFactor} and vertically by ${yFactor}`
+      }`,
+    });
+
+    this._vertices = img.vertices.map((v) => ({
+      x: v.x * xFactor,
+      y: v.y * yFactor,
+    }));
+    this.xFactor = xFactor;
+    this.yFactor = yFactor;
+
+    if (xFactor < 0) {
+      // translate pinhole into image region
+      this.pinholeX += this.width;
+      this.vertices.forEach((v) => (v.x += this.width));
+    }
+
+    if (yFactor < 0) {
+      // translate pinhole into image region
+      this.pinholeY += this.height;
+      this.vertices.forEach((v) => (v.y += this.height));
+    }
+  }
+
+  /**
+   * Static constructor for ScaleImage
+   * @param {number} xFactor
+   * @param {number} yFactor
+   * @param {BaseImage} img
+   */
+  static new(xFactor, yFactor, img) {
+    return new ScaleImage(xFactor, yFactor, img);
+  }
+
+  /**
+   * Equality check for ScaleImage
+   * @param {BaseImage} other
+   * @returns {boolean}
+   */
+  equals(other) {
+    return (
+      (other instanceof ScaleImage &&
+        this.width === other.width &&
+        this.height === other.height &&
+        this.xFactor === other.xFactor &&
+        this.yFactor === other.yFactor &&
+        imageEquals(this.img, other.img)) ||
+      BaseImage.prototype.equals.call(this, other)
+    );
+  }
+
+  /**
+   * Renders a ScaleImage to the screen
+   * @param {CanvasRenderingContext2D} ctx
+   */
+  render(ctx) {
+    ctx.save();
+    ctx.scale(this.xFactor, this.yFactor);
+
+    if (this.xFactor < 0) {
+      ctx.translate(this.width / this.xFactor, 0);
+    }
+
+    if (this.yFactor < 0) {
+      ctx.translate(0, this.height / this.yFactor);
+    }
+
+    this.img.render(ctx);
+    ctx.restore();
+  }
+}
