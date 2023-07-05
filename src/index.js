ee72d1c0971f68cdd36d40076bf5dd4973a3c3a0
Jason Barr <jason@jasonsbarr.com>
2023-07-04 22:28:43 -0500

    Add render function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import * as ImageLib from "./image-lib/index.js";
+import { makeDocument } from "./shared/document.js";
+
+const document = makeDocument();
+
+/**
+ * Renders an image to the DOM
+ * @param {ImageLib.BaseImage} image
+ * @param {HTMLElement} element
+ */
+export const render = (image, element = document.body) => {
+  const canvas = ImageLib.makeCanvas(image.width, image.height);
+  const context = canvas.getContext("2d");
+
+  element.appendChild(canvas);
+  image.render(context);
+};
 
 export { ImageLib };
