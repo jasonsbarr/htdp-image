477188ed73d58c5ba63344a3867f6789d972c3ef
Jason Barr <jason.barr@metronet.com>
2023-07-22 18:26:24 -0500

    Add utility functions and export shared types

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import * as ImageLib from "./image-lib/index.js";
+import {
+  FillMode,
+  FontFamily,
+  FontStyle,
+  FontWeight,
+  Point,
+  Point2D,
+  point,
+  XPlace,
+  YPlace,
+} from "./shared/index.js";
 import { makeDocument } from "./shared/document.js";
 
 const document = makeDocument();
@@ -16,4 +27,49 @@ export const render = (image, element = document.body) => {
   image.render(context);
 };
 
-export { ImageLib };
+/**
+ * Checks if a given string matches a color in the color DB
+ * @param {string} color
+ * @returns {boolean}
+ */
+export const isImageColor = (color) => ImageLib.isColorOrColorString(color);
+
+/**
+ * Checks if the value is a FillMode
+ * @param {any} mode
+ * @returns {boolean}
+ */
+export const isMode = (mode) => FillMode.isFillMode(mode);
+
+/**
+ * Checks if a given value is a PlaceX value
+ * @param {any} x
+ * @returns {boolean}
+ */
+export const isXPlace = (x) =>
+  typeof x === "string" &&
+  (x.toLowerCase() === "left" ||
+    x.toLowerCase() === "right" ||
+    x.toLowerCase() === "center" ||
+    x.toLowerCase() === "pinhole" ||
+    x.toLowerCase() === "middle");
+
+export const isYPlace = (x) =>
+  typeof x === "string" &&
+  (x.toLowerCase() === "top" ||
+    x.toLowerCase() === "bottom" ||
+    x.toLowerCase() === "baseline" ||
+    x.toLowerCase() === "center" ||
+    x.toLowerCase() === "pinhole" ||
+    x.toLowerCase() === "middle");
+
+export {
+  FontFamily,
+  FontStyle,
+  FontWeight,
+  Point,
+  Point2D,
+  point,
+  XPlace,
+  YPlace,
+};
